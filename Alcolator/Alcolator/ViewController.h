bf5b4c4f6ef675c4c0fd90a3f8ed545fca518b35
Ben Neely <ben@bloc.io>
2015-05-14 20:54:46 -0600

    Initial commit and storyboard

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Ben Neely on 5/14/15.
+//  Copyright (c) 2015 Ben Neely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
