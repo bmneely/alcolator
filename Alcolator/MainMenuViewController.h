47e609d8979c492231965041b0cf96cc53ed60da
Ben Neely <ben@bloc.io>
2015-05-18 22:09:35 -0600

    Complete basic navigation

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Ben Neely on 5/18/15.
+//  Copyright (c) 2015 Ben Neely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
