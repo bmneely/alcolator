04545453235836502c9710842efe16e55cb8de5f
Ben Neely <ben@bloc.io>
2015-10-18 11:01:37 -0600

    8 | Storyboard Alcolator checkpoint

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Ben Neely on 10/18/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
